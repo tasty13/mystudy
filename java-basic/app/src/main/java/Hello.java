41a111c4b24aab4284d5f739a4a01674b714582c
tasty13 <bodri0625@gmail.com>
2023-11-21 18:31:53 +0900

    231121

--- java-basic/app/src/main/java/Hello.java
+++ java-basic/app/src/main/java/Hello.java
@@ -2,8 +2,9 @@
  * Hello
  */
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("안녕하세요");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("안녕하세요!");
+    System.out.println("안녕하세요!2222");
+  }
 }
