df22ee5676fa35ec72aaf08a3a336740a45ecc2a
tasty13 <bodri0625@gmail.com>
2023-12-27 16:49:12 +0900

    231227

--- java-basic/myapp/app/src/main/java/bitcamp/util/LinkedList.java
+++ java-basic/myapp/app/src/main/java/bitcamp/util/LinkedList.java
@@ -1,12 +1,17 @@
 package bitcamp.util;
 
-public class LinkedList {
+public class LinkedList<E> {
 
-  Node first;
-  Node last;
+  private Node<E> first;
+  private Node<E> last;
+  private int size;
 
-  public void add(Object value) {
-    Node node = new Node();
+  public int size() {
+    return size;
+  }
+
+  public void add(E value) {
+    Node<E> node = new Node<>();
     node.value = value;
 
     if (last == null) {
@@ -17,5 +22,148 @@ public class LinkedList {
       last.next = node; // last 노드의 next에 node 주소 넣음
       last = node; // last 주소를 새로 만든 노드 주소로
     }
+    size++;
+  }
+
+  public Object[] toArray() {
+    Object[] arr = new Object[size];
+    int index = 0;
+    Node<E> node = first;
+    while (node != null) {
+      arr[index++] = node.value;
+      node = node.next;
+    }
+    return arr;
+  }
+
+  public E get(int index) {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("무효한 인덱스입니다.");
+    }
+
+    int cursor = 0;
+    Node<E> node = first;
+    while (++cursor <= index) {
+      node = node.next;
+    }
+    return node.value;
+  }
+
+  public E set(int index, E value) {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("무효한 인덱스입니다.");
+    }
+
+    int cursor = 0;
+    Node<E> node = first;
+    while (++cursor <= index) {
+      node = node.next;
+    }
+
+    E old = node.value;
+    node.value = value;
+    return old;
+  }
+
+  public void add(int index, E value) {
+    if (index < 0 || index > size) {
+      throw new IndexOutOfBoundsException("무효한 인덱스입니다.");
+    }
+
+    Node<E> node = new Node<>();
+    node.value = value;
+
+    if (first == null) {
+      first = last = node;
+    } else if (index == 0) {
+      // index가 0일 때 -> 원래 first였던 값이랑 새 노드 연결, 새 노드를 first로
+      node.next = first;
+      first = node;
+
+    } else if (index == size) {
+      // index가 size일 때-> 원래 마지막 노드가 새 노드 가리키게, 새 노드를 last로
+      last.next = node;
+      last = node;
+    } else {
+      // 리스트에서 삽입할 인덱스의 바로 전 노드에 해당하는 값을 찾음
+      // 얘 next 주소를 새 노드에, 얘 next에 새 노드 주소 넣어주기
+      int cursor = 0;
+      Node<E> currNode = first;
+      while (++cursor < index) {
+        currNode = currNode.next;
+      }
+      node.next = currNode.next;
+      currNode.next = node;
+    }
+    size++;
+  }
+
+  public E remove(int index) {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("무효한 인덱스입니다.");
+    }
+
+    E old = null;  // 지워질 예정인 노드를 저장해서 리턴하기
+
+    if (size == 1) {
+      // 1개일 때
+      old = first.value;
+      first = last = null;
+
+    } else if (index == 0) {
+      // 맨 처음 삭제
+      old = first.value;
+      first = first.next;
+
+    } else {
+      int cursor = 0;
+      Node<E> currNode = first;
+      while (++cursor < index) {
+        currNode = currNode.next;
+      }
+      old = currNode.next.value;
+      currNode.next = currNode.next.next;
+
+      if (index == size - 1) {
+        // 마지막 node 삭제할 때 last 바꿔줌
+        last = currNode;
+      }
+    }
+    size--;
+    return old;
+  }
+
+  public boolean remove(E value) {
+    Node prevNode = null;
+    Node node = first;
+
+    while (node != null) {
+      if (node.value.equals(value)) {
+        break;
+      }
+      prevNode = node;
+      node = node.next;
+    }
+    if (node == null) {
+      return false;
+    }
+
+    if (node == first) {
+      first = first.next; // 다음노드를 첫번째노드로. node.next를 썼을때보다 가독성이 좋다
+      if (first == null) {
+        last = null;
+      }
+    } else {
+      prevNode.next = node.next;
+    }
+
+    size--;
+    return true;
+  }
+
+  private static class Node<E> {
+
+    E value;
+    Node<E> next;
   }
 }
--- java-basic/myapp/app/src/main/java/bitcamp/util/Test.java
+++ java-basic/myapp/app/src/main/java/bitcamp/util/Test.java
@@ -3,22 +3,47 @@ package bitcamp.util;
 public class Test {
 
   public static void main(String[] args) {
-    LinkedList list = new LinkedList();
-    list.add(new String("aaa"));
-    list.add(new String("bbb"));
-    list.add(new String("ccc"));
-    list.add(new String("ddd"));
+    LinkedList<String> list = new LinkedList<>();
+    list.add("aaa"); // aaa,
+    list.add("bbb"); // aaa, bbb,
+    list.add("ccc"); // aaa, bbb, ccc,
+    list.add("ddd"); // aaa, bbb, ccc, ddd,
 
-    Node node = list.first; // 첫번째 노드
-    System.out.println(node.value);
+    System.out.println(list.remove("xxx")); // aaa, bbb, ccc, ddd,
+    System.out.println(list.remove("ccc")); // aaa, bbb, ddd,
+    System.out.println(list.remove("ddd")); // aaa, bbb,
+    System.out.println(list.remove("aaa")); // bbb,
+    System.out.println(list.remove("bbb")); //
+    list.add("xxx"); // xxx,
+    list.add("yyy"); // xxx, yyy,
+    list.add("zzz"); // xxx, yyy, zzz,
 
-    node = node.next;
-    System.out.println(node.value);
+//    list.remove(2); // aaa, bbb, ddd,
+//    list.remove(2); // aaa, bbb,
+//    list.remove(0); // bbb,
+//    list.remove(0); //
 
-    node = node.next;
-    System.out.println(node.value);
+//    // 맨 앞
+//    list.add(0, new String("xxx")); // xxx, aaa, bbb, ccc, ddd,
+//
+//    // 맨 뒤
+//    list.add(5, new String("yyy")); // xxx, aaa, bbb, ccc, ddd, yyy,
+//
+//    // 기존 값 자리
+//    list.add(1, new String("mmm")); // xxx, mmm, aaa, bbb, ccc, ddd, yyy,
+//    list.add(3, new String("ttt")); // xxx, mmm, aaa, ttt, bbb, ccc, ddd, yyy,
+//    list.add(7, new String("ppp")); // xxx, mmm, aaa, ttt, bbb, ccc, ddd, ppp, yyy,
 
-    node = node.next;
-    System.out.println(node.value);
+    Object[] arr = list.toArray();
+    for (Object value : arr) {
+      System.out.printf("%s, ", value);
+    }
+    System.out.println();
+
+//    System.out.println(list.get(0));
+//    System.out.println(list.get(1));
+//    System.out.println(list.get(2));
+//    System.out.println(list.get(3));
+//    System.out.println(list.get(4));
   }
 }
