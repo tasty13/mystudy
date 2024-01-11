5fbcc12feab6b050f0cd68499ad2ae93bfe2647f
tasty13 <bodri0625@gmail.com>
2024-01-11 17:41:32 +0900

    240111

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam05/Main.java
@@ -3,23 +3,13 @@ package algorithm.test.baekjoon.level06.exam05;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
-  public static int countChar(String str, String string) {
-    return str.length() - str.replace(String.valueOf(string), "").length();
-  }
-
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String str = br.readLine().toUpperCase();
-
-    // 1. str배열이랑 count배열
-    // 2. str 한글자씩 읽음, 해당 글자가 있는지 검사
-    // 2-1. 배열에 글자가 없으면 배열에 추가, 해당 인덱스 count+1
-    // 2-1. 있으면 해당 인덱스 count+1만
-    // 3. int max, Char c
-    // 4. max와 같은 count가 있다면 ? 출력, 아니라면 c 출력
 
     List<Character> strList = new ArrayList<>();
     List<Integer> counts = new ArrayList<>();
@@ -30,30 +20,24 @@ public class Main {
       if (!strList.contains(c)) {
         strList.add(c);
         counts.add(1);
+        if (max < 1) {
+          max = 1;
+        }
       } else {
-        int count = counts.get(strList.indexOf(c));
-        counts.set(strList.indexOf(c), count++);
-        if (max > count) {
+        int index = strList.indexOf(c);
+        int count = counts.get(index);
+        counts.set(index, ++count);
+        if (max < count) {
           max = count;
         }
       }
     }
 
-    strList.forEach(System.out::print);
-    System.out.println();
-    for (int i : counts) {
-      System.out.print(i);
-    }
-
     Character result = null;
-    for (int i = 0; i <= counts.size(); i++) {
-      if (counts.get(i) == max) {
-        result = '?';
-        break;
-      }
-      if (counts.get(i) > max) {
-        result = strList.get(i);
-      }
+    if (Collections.frequency(counts, max) >= 2) {
+      result = '?';
+    } else {
+      result = strList.get(counts.indexOf(max));
     }
     System.out.print(result);
   }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam01/Main.java
@@ -1,38 +1,35 @@
 package algorithm.test.baekjoon.level07.exam01;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
 public class Main {
   public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-    int N = Integer.parseInt(st1.nextToken());
-    int M = Integer.parseInt(st1.nextToken());
+    final int N = getInt();
+    final int M = getInt();
 
-    int[][] arr = setArr(br, arr, N, M);
-    arr = setArr(br, arr, N, M);
-
+    final int[][] arr = new int[N][M];
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        arr[i][j] = getInt();
+      }
+    }
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
-        sb.append(arr[i][j]).append(" ");
+        arr[i][j] += getInt();
+        sb.append(arr[i][j]).append(' ');
       }
-      sb.append("\n");
+      sb.append('\n');
     }
     System.out.print(sb);
 
   }
 
-  // \n이나 ' ' 안만나면 받은 원래값에 *10
-  static int[][] setArr(BufferedReader br, int[][] arr, int N, int M) throws Exception {
-    for (int i = 0; i < N; i++) {
-      StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-      for (int j = 0; j < M; j++) {
-        arr[i][j] += Integer.parseInt(st2.nextToken());
-      }
+  // 이클립스에서실행이안됨...........................
+  static int getInt() throws Exception {
+    int result = 0;
+    int num = 0;
+    while ((num = System.in.read()) != '\n' && num != ' ') {
+      result = result * 10 + (num - '0');
     }
-    return arr;
+    return result;
   }
 }
