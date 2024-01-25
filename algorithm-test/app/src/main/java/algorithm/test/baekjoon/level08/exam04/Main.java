6a949c98145460a2cd30a870a489234f60ff0115
tasty13 <bodri0625@gmail.com>
2024-01-25 21:39:46 +0900

    240125

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam04/Main.java
@@ -1,7 +1,13 @@
 package algorithm.test.baekjoon.level08.exam04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(br.readLine());
+    int r = (int) (Math.pow(2, num));
+    System.out.println(4 * 1 + 2 * (r - 1) * 2 + 1 * (r - 1) * (r - 1));
   }
 }
