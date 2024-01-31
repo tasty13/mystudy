09dfb1237da5481493093aae44121f719afc1b4f
tasty13 <bodri0625@gmail.com>
2024-01-31 14:01:32 +0900

    240131

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level09/exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level09/exam02/Main.java
@@ -2,10 +2,9 @@ package algorithm.test.baekjoon.level09.exam02;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,21 +15,16 @@ public class Main {
     int b = Integer.parseInt(st.nextToken());
 
     int m = (int) Math.sqrt(a);
-    Set<Integer> set = new HashSet<>();
+    List<Integer> list = new ArrayList<>();
     for (int i = 1; i <= m; i++) {
       if (a % i == 0) {
-        set.add(i);
-        set.add(a / i);
-        System.out.printf("a: %d, a / i = %d\n", i, a / i);
+        if (i != a / i) {
+          list.add(a / i);
+        }
+        list.add(i);
       }
     }
-
-    List<Integer> list = List.copyOf(set);
-    Collections.sort(list);
-    for (int i : list) {
-      System.out.printf("%d ", i);
-    }
-    System.out.println();
+    list.sort(Comparator.naturalOrder());
 
     if (list.size() < b) {
       System.out.println(0);
