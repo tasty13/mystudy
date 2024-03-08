419aa769bb2ffc4f0ce18f10313297bcf41cfd91
tasty13 <bodri0625@gmail.com>
2024-03-08 11:24:14 +0900

    240308:

--- java-basic/myapp/app/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ java-basic/myapp/app/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -7,8 +7,12 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+@Controller
 public class AuthController {
 
   MemberDao memberDao;
--- java-basic/myapp/app/src/main/java/bitcamp/myapp/controller/MemberController.java
+++ java-basic/myapp/app/src/main/java/bitcamp/myapp/controller/MemberController.java
@@ -7,8 +7,11 @@ import java.util.Map;
 import java.util.UUID;
 import javax.servlet.http.Part;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+@Controller
 public class MemberController {
 
   private MemberDao memberDao;
