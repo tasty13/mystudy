123419e91082f99d8e4ab67502125224e1e77d68
tasty13 <bodri0625@gmail.com>
2024-03-11 15:46:27 +0900

    240311

--- java-basic/app/src/main/java/com/eomcs/spring/ioc/ex01/e/AppConfig.java
+++ java-basic/app/src/main/java/com/eomcs/spring/ioc/ex01/e/AppConfig.java
@@ -1,6 +1,5 @@
 package com.eomcs.spring.ioc.ex01.e;
 
-import org.springframework.context.annotation.Bean;
 import com.eomcs.spring.ioc.ex01.Car;
 
 // 클래스 선언부에 애노테이션으로 스프링 설정에 관한 정보를 지정할 수 있다.
--- java-basic/app/src/main/java/com/eomcs/spring/ioc/ex04/e/Exam01.java
+++ java-basic/app/src/main/java/com/eomcs/spring/ioc/ex04/e/Exam01.java
@@ -4,6 +4,7 @@ package com.eomcs.spring.ioc.ex04.e;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.eomcs.spring.ioc.ex04.Car;
+import com.eomcs.spring.ioc.ex04.Engine;
 
 public class Exam01 {
 
@@ -15,6 +16,10 @@ public class Exam01 {
     Car c2 = (Car) iocContainer.getBean("c2");
 
     System.out.println(c1.getEngine() == c2.getEngine());
+
+    // 프로퍼티 생성 중 만드는 객체는 컨테이너에 따로 보관되지 않음
+    // -> 따라서 그 객체 선언할 땐 id 지정 필요 X
+    Engine e1 = (Engine) iocContainer.getBean("e1");
   }
 
 }
