4a3701893d7f2fa961b83f7d1661a0dbdef7d879
tasty13 <bodri0625@gmail.com>
2024-03-13 12:13:28 +0900

    240312

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level10/exam05/Main.java
@@ -0,0 +1,14 @@
+package algorithm.test.baekjoon.level10.exam05;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) {
+    // 세로-> 쌓인사각형갯수*2
+    // 가로-> 밑변사각형갯수*2
+    // long
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  }
+}
