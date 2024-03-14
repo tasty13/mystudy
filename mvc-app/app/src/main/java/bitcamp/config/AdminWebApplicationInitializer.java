94b0d7e9b8a790fea6638b44860cad56f111ccd6
tasty13 <bodri0625@gmail.com>
2024-03-14 18:07:38 +0900

    240314

---
+++ mvc-app/app/src/main/java/bitcamp/config/AdminWebApplicationInitializer.java
@@ -0,0 +1,47 @@
+package bitcamp.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class AdminWebApplicationInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer {
+
+  private static Log log = LogFactory.getLog(AdminWebApplicationInitializer.class);
+  AnnotationConfigWebApplicationContext rootContext;
+
+  @Override
+  protected WebApplicationContext createRootApplicationContext() {
+    rootContext = new AnnotationConfigWebApplicationContext();
+    rootContext.register(RootConfig.class);
+    rootContext.refresh();
+    return rootContext;
+  }
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[] {RootConfig.class};
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] {AdminConfig.class};
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] {"/admin/*"};
+  }
+
+  @Override
+  protected String getServletName() {
+    return "admin";
+  }
+}
