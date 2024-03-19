878bc771e3b4914dd82438d6ea6e05074e1641df
tasty13 <bodri0625@gmail.com>
2024-03-19 10:39:48 +0900

    240319

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/exam01/Main.java
@@ -0,0 +1,10 @@
+package algorithm.test.baekjoon.level11.exam01;
+
+public class Main {
+  public static void main(String[] args) {
+    // 수행 횟수는 항상 1번 => t = 1
+    // 최고차항의 차수도 항상 0
+    System.out.println(1);
+    System.out.println(0);
+  }
+}
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/exam02/Main.java
@@ -0,0 +1,13 @@
+package algorithm.test.baekjoon.level11.exam02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    // 수행횟수 = n
+    // 최고차항 차수 = 1
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println(br.readLine() + "\n" + 1);
+  }
+}
