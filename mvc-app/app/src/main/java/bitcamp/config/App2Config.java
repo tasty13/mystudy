9dca82a1610f55febd14a50072286fe8177b7551
tasty13 <bodri0625@gmail.com>
2024-03-20 19:08:33 +0900

    240320

--- mvc-app/app/src/main/java/bitcamp/config/App2Config.java
+++ mvc-app/app/src/main/java/bitcamp/config/App2Config.java
@@ -1,11 +1,16 @@
 package bitcamp.config;
 
+import bitcamp.app2.Interceptor1;
+import bitcamp.app2.Interceptor2;
+import bitcamp.app2.Interceptor3;
+import bitcamp.app2.Interceptor4;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -29,5 +34,24 @@ public class App2Config implements WebMvcConfigurer {
     UrlPathHelper urlPathHelper = new UrlPathHelper();
     urlPathHelper.setRemoveSemicolonContent(false); // url ; 뒤 컨텐츠 삭제할갑쇼
     configurer.setUrlPathHelper(urlPathHelper);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    // 인터셉터를 적용할 URL을 지정하지 않으면 모든 request handler에 대해 적용된다.
+    registry.addInterceptor(new Interceptor1());
+    // 1) 모든 요청에 대해 실행할 인터셉터 등록하기
+    // => 인터셉터를 적용할 URL을 지정하지 않으면
+    // 현재 프론트 컨트롤러의 모든 요청에 대해 적용된다.
+    //
+    registry.addInterceptor(new Interceptor2())
+        .addPathPatterns("/c04_1/*");
+
+    registry.addInterceptor(new Interceptor3())
+        .addPathPatterns("/c04_1/**");
+
+    registry.addInterceptor(new Interceptor4())
+        .addPathPatterns("/c04_1/**")
+        .excludePathPatterns("/c04_1/a/**");
   }
 }
