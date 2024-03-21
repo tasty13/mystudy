bb0fd0eeb6f8838547d11bfe80f09fd620382f4c
tasty13 <bodri0625@gmail.com>
2024-03-21 17:32:54 +0900

    240321

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/exam03/Main.java
@@ -0,0 +1,13 @@
+package algorithm.test.baekjoon.level11.exam03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    // n을 내부에서 n만큼 돌림 -> 수행횟수 n^2 => 최고차항 차수 2
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int num = Integer.parseInt(br.readLine());
+    System.out.println(num * num + "\n" + 2);
+  }
+}
