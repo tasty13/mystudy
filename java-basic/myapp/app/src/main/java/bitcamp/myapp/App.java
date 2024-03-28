c299b303611f9e82fad9f969a777e0129014dccb
tasty13 <bodri0625@gmail.com>
2024-03-28 16:05:08 +0900

    240328

--- java-basic/myapp/app/src/main/java/bitcamp/myapp/App.java
+++ java-basic/myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,20 +1,33 @@
 package bitcamp.myapp;
 
-import java.io.File;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.connector.Connector;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@PropertySource({
+    "classpath:config/ncp.properties",
+    "classpath:config/ncp-secret.properties"})
+@EnableTransactionManagement
 @SpringBootApplication
+@Controller
 public class App {
 
   public static void main(String[] args) throws Exception {
     System.out.println("과제관리 시스템 서버 실행!");
     SpringApplication.run(App.class, args);
   }
+
+  @GetMapping("/home")
+  public void home() {
+    // return "home"; // ==> ThymeleafViewResolver가 처리한다.
+  }
+
+  @GetMapping("/about")
+  public void about() {
+  }
 }
+
+
