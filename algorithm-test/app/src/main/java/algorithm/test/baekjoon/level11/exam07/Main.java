05424dbaafa92b9c16a347ee17ff83c80609581c
tasty13 <bodri0625@gmail.com>
2024-04-06 23:51:45 +0900

    240406

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/exam07/Main.java
@@ -0,0 +1,22 @@
+package algorithm.test.baekjoon.level11.exam07;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int a1 = Integer.parseInt(st.nextToken());
+    int a0 = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(br.readLine());
+    int n0 = Integer.parseInt(br.readLine());
+
+    if (n0 >= (a0 / (c - a1))) {
+      System.out.println(1);
+    } else {
+      System.out.println(0);
+    }
+  }
+}
