c60efdb757abb281ecf7bc8e29db086e2ceaad56
tasty13 <bodri0625@gmail.com>
2024-04-14 00:09:26 +0900

    240414

--- algorithm-test/app/src/main/java/algorithm/test/jcp/chap01/exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/jcp/chap01/exam05/Main.java
@@ -1,16 +1,21 @@
 package algorithm.test.jcp.chap01.exam05;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.commons.lang3.tuple.Pair;
 
 // 모음과 자음 세기
 public class Main {
   private static final Set<Character> allVowels =
       new HashSet(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Pair<Integer, Integer> result = countVowelsAndConsonants(br.readLine());
+    System.out.println(result);
   }
 
   // Pair 사용 -> apache commons 라이브러리 사용
